3773380af49747363dfb95b639810172e8951ae9
dohwi <dohwe0978@gmail.com>
2025-11-06 05:23:38 +0900

    feat :: feign client 추가

--- masil/src/main/kotlin/sw2025/masil/MasilApplication.kt
+++ masil/src/main/kotlin/sw2025/masil/MasilApplication.kt
@@ -3,8 +3,10 @@ package sw2025.masil
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @ConfigurationPropertiesScan
+@EnableFeignClients
 @SpringBootApplication
 class MasilApplication
 
