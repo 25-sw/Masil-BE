18d9b4009a5eb036783f06058583263216f21dcb
dohwi <dohwe0978@gmail.com>
2025-11-05 23:46:46 +0900

    feat :: jwt auth

---
+++ masil/src/main/kotlin/sw2025/masil/global/security/auth/AuthDetails.kt
@@ -0,0 +1,36 @@
+package sw2025.masil.global.security.auth
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class AuthDetails(
+    private val accountId: String
+) : UserDetails {
+    override fun getAuthorities(): Collection<GrantedAuthority?>? {
+        return null
+    }
+
+    override fun getPassword(): String? {
+        return null
+    }
+
+    override fun getUsername(): String {
+        return accountId
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+}
