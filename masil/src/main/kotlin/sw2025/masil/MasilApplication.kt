b9ea4fb355efb5c569a184cc7d4c62c9abf16770
dohwi <dohwe0978@gmail.com>
2025-11-05 20:15:27 +0900

    init :: project 생성

---
+++ masil/src/main/kotlin/sw2025/masil/MasilApplication.kt
@@ -0,0 +1,11 @@
+package sw2025.masil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MasilApplication
+
+fun main(args: Array<String>) {
+	runApplication<MasilApplication>(*args)
+}
