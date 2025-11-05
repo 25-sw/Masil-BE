d09f2b426127e64d6ae72d78dfbd53d8d0baf763
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-05 21:57:14 +0900

    update :: JPA 및 Redis 의존성 추가

--- masil/build.gradle.kts
+++ masil/build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
 }
 
 kotlin {
