34399cf2124fdec080e166e0147ba174a59bc5ad
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-05 22:01:25 +0900

    add :: Poster 추가

---
+++ masil/src/main/kotlin/sw2025/masil/domain/poster/entity/Poster.kt
@@ -0,0 +1,22 @@
+package sw2025.masil.domain.poster.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import sw2025.masil.domain.storeowner.entity.StoreOwner
+import sw2025.masil.global.entity.BaseEntity
+
+@Entity
+@Table(name = "poster")
+class Poster (
+
+    @Column(name = "file_name", nullable = false, columnDefinition = "VARCHAR(512)")
+    val fileName: String,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_owner_id", nullable = false, columnDefinition = "BINARY(16)")
+    val storeOwner: StoreOwner,
+) : BaseEntity()
