ada3e281a454c495656bfa148738f758417d9f70
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-05 22:00:37 +0900

    add :: docker compose 설정 파일 추가

--- masil/build.gradle.kts
+++ masil/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
 
 kotlin {
