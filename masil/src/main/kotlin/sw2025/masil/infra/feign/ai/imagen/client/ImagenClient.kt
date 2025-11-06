8b3878951ed22fddca6def0eaf2c3334610bc068
suuuuuuminnnnnn <sumin45402214@gmail.com>
2025-11-06 20:24:06 +0900

    update :: 불필요한 import 제거

--- masil/src/main/kotlin/sw2025/masil/infra/feign/ai/imagen/client/ImagenClient.kt
+++ masil/src/main/kotlin/sw2025/masil/infra/feign/ai/imagen/client/ImagenClient.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
-import org.springframework.web.bind.annotation.RequestParam
 import sw2025.masil.infra.feign.ai.imagen.dto.req.ImagenPromptRequest
 import sw2025.masil.infra.feign.ai.imagen.dto.res.ImagenResponse
 
